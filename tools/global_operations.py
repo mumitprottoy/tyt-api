8b1526e62227a76a715e23f367d26a3363e4884f
Mumit Prottoy <mumitprottoy@gmail.com>
2023-11-19 23:48:33 +0600

    Delete tools directory

--- tools/global_operations.py
+++
@@ -1,7 +0,0 @@
-def passes_check_list(check_list: tuple[bool, str]) -> tuple[bool, str]:
-    issues = list()
-    for check in check_list:
-        if not check[0]: issues.append(check)
-    
-
-    return (True, None) if not issues else (False, issues)
